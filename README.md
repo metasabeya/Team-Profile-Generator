db92e5a26e6bf1ea7f0dc822a999013fad0d2ea6
unknown <metybuza46@gmail.com>
2020-08-26 18:59:31 -0600

    added asset folder

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Team-Profile-Generator
+
+
+https://drive.google.com/file/d/1mkGhIJ9tbQaABWkfhCVmZI9sCIi_yf5Y/view?usp=sharing
--- app.js
+++ app.js
@@ -156,6 +156,9 @@ function resume() {
     });
 }
 resume();
+
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
